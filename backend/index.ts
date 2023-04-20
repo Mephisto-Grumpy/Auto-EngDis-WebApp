5a1c8cc3143f208eb59ffd383f5dd5ed11237ba8
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-04-20 19:39:11 +0700

    :art: prettier

--- backend/index.ts
+++ backend/index.ts
@@ -1,61 +1,61 @@
-import express from "express";
-import axios from "axios";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express from 'express'
+import axios from 'axios'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
 
-const ENGDIS_BASE_API_URL = "https://eduiwebservices20.engdis.com/api/";
+const ENGDIS_BASE_API_URL = 'https://eduiwebservices20.engdis.com/api/'
 
-app.get("/", (req, res, next) => {
-  res.json({ hello: "world" });
-});
+app.get('/', (req, res, next) => {
+  res.send('This is Engdis API Proxy Server')
+})
 
-app.post("/apis/Auth/Login", (req, res, next) => {
+app.post('/apis/Auth/Login', (req, res, next) => {
   axios
-    .post(ENGDIS_BASE_API_URL + "Auth/forcelogin", {
-      CommunityVersion: "100",
-      InstitutionId: "5232957", // KMITL
+    .post(ENGDIS_BASE_API_URL + 'Auth/forcelogin', {
+      CommunityVersion: '100',
+      InstitutionId: '5232957', // KMITL
       Password: req.body.password,
-      UserName: req.body.username,
+      UserName: req.body.username
     })
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-app.post("/apis/Auth/Logout", (req, res, next) => {
+app.post('/apis/Auth/Logout', (req, res, next) => {
   const config = {
-    headers: { Authorization: `Bearer ${req.body.token}` },
-  };
+    headers: { Authorization: `Bearer ${req.body.token}` }
+  }
   axios
     .get(`${ENGDIS_BASE_API_URL}Auth/Logout`, config)
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-app.post("/apis/CourseTree/GetDefaultCourseProgress", (req, res, next) => {
+app.post('/apis/CourseTree/GetDefaultCourseProgress', (req, res, next) => {
   const config = {
-    headers: { Authorization: `Bearer ${req.body.token}` },
-  };
+    headers: { Authorization: `Bearer ${req.body.token}` }
+  }
   axios
     .get(`${ENGDIS_BASE_API_URL}CourseTree/GetDefaultCourseProgress`, config)
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-app.post("/apis/CourseTree/getCourseTree", (req, res, next) => {
+app.post('/apis/CourseTree/getCourseTree', (req, res, next) => {
   const config = {
-    headers: { Authorization: `Bearer ${req.body.token}` },
-  };
+    headers: { Authorization: `Bearer ${req.body.token}` }
+  }
 
   const data = [
     {
@@ -63,9 +63,9 @@ app.post("/apis/CourseTree/getCourseTree", (req, res, next) => {
       NodeType: 2,
       LockedNodes: null,
       particleHasProgress: true,
-      lowestNodeType: 5,
-    },
-  ];
+      lowestNodeType: 5
+    }
+  ]
 
   axios
     .post(
@@ -73,34 +73,34 @@ app.post("/apis/CourseTree/getCourseTree", (req, res, next) => {
       data,
       config
     )
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-app.post("/apis/Progress/SetProgressPerTask", (req, res, next) => {
+app.post('/apis/Progress/SetProgressPerTask', (req, res, next) => {
   const config = {
-    headers: { Authorization: `Bearer ${req.body.token}` },
-  };
+    headers: { Authorization: `Bearer ${req.body.token}` }
+  }
 
   const data = {
     CourseId: req.body.CourseId,
-    ItemId: req.body.ItemId,
-  };
+    ItemId: req.body.ItemId
+  }
 
   axios
     .post(`${ENGDIS_BASE_API_URL}Progress/SetProgressPerTask`, data, config)
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-app.post("/apis/UserTestV1/SaveUserTest", (req, res, next) => {
+app.post('/apis/UserTestV1/SaveUserTest', (req, res, next) => {
   const config = {
-    headers: { Authorization: `Bearer ${req.body.token}` },
-  };
+    headers: { Authorization: `Bearer ${req.body.token}` }
+  }
 
   axios
     .post(
@@ -108,14 +108,14 @@ app.post("/apis/UserTestV1/SaveUserTest", (req, res, next) => {
       req.body.payload,
       config
     )
-    .then((response) => {
-      res.json(response.data);
+    .then(response => {
+      res.json(response.data)
     })
-    .catch((error) => console.log(error));
-});
+    .catch(error => console.log(error))
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server are now started on port: ${PORT}`);
-});
+  console.log(`Server are now started on port: ${PORT}`)
+})
